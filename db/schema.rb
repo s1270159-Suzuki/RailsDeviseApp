7d79f5bc8281d2367dac1cd2a408e2c5d54f0a17
Haruya Suzuki <nuanyelingmu@gmail.com>
2020-11-16 12:16:53 +0900

    create board_messages

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_13_153833) do
+ActiveRecord::Schema.define(version: 2020_11_14_173732) do
 
   create_table "accounts", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -22,6 +22,21 @@ ActiveRecord::Schema.define(version: 2020_11_13_153833) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_accounts_on_email", unique: true
     t.index ["reset_password_token"], name: "index_accounts_on_reset_password_token", unique: true
+  end
+
+  create_table "board_messages", force: :cascade do |t|
+    t.text "content"
+    t.integer "board_user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "board_users", force: :cascade do |t|
+    t.text "nickname"
+    t.integer "account_id"
+    t.text "memo"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "data_pages", force: :cascade do |t|
